200ee9c1a56615f7f1bf69803e172bc2842d906c
Lifeof31415926535 <tpray84@gmail.com>
2023-03-04 15:33:38 -0600

    Replaced index reader and writer modules with index source module.

---
+++ src/abstracts/index_sources.py
@@ -0,0 +1,63 @@
+from abc import ABC, abstractmethod
+
+from .downloaders import Downloader
+
+
+class IndexFileReader(ABC):
+    index_type: str
+
+    @abstractmethod
+    def read(self, index_data) -> list[str]:
+        pass
+
+
+class IndexWriter(ABC):
+    index_type: str
+
+    @abstractmethod
+    def write(self, sources):
+        pass
+
+
+class IndexFileManager(ABC):
+    index_type: str
+
+    def __init_subclass__(cls, **kwargs):
+        index_type = kwargs.get('index_type')
+        if index_type is None:
+            raise Exception()
+        cls.index_type = index_type
+
+        for factory_type in ['downloader_factory', 'reader_factory', 'writer_factory']:
+            factory = kwargs.get(factory_type)
+            if factory is None:
+                raise Exception(f"{factory_type}")
+            setattr(cls, factory_type, factory)
+
+    def __init__(self, downloader: str = 'default', reader: str = "default", writer: str = "default"):
+        self.downloader = self.downloader_factory(downloader)
+        self.reader = self.reader_factory(reader)
+        self.writer = self.writer_factory(writer)
+        self._errors = []
+        self._source = None
+        self._count = 0
+
+    @abstractmethod
+    def run(self, url: str):
+        pass
+
+    @property
+    def errors(self) -> list[dict]:
+        return self._errors
+
+    @property
+    def failed(self) -> int:
+        return len(self.errors)
+
+    @property
+    def successful(self) -> int:
+        return self._count - self.failed
+
+    #@property
+    #def source(self):
+     #   return self._source
--- src/abstracts/index_writers.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class IndexWriter(ABC):
-    index_type: str
-
-    @abstractmethod
-    def write(self, sources):
-        pass
