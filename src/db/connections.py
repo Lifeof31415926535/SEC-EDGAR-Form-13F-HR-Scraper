82fdd8c722b692ded164c431da666cdd3068919e
Lifeof31415926535 <tpray84@gmail.com>
2023-03-04 15:38:59 -0600

    Fixed a bug by removing an invalid attribute reference.

--- src/db/connections.py
+++ src/db/connections.py
@@ -4,16 +4,16 @@ from config import DB_CONFIG
 
 
 class ConnectionManager:
-    def __init__(self, profile=DB_CONFIG):
+    def __init__(self, profile: dict):
         self.profile = profile
 
     def __enter__(self):
-        print(f"Opening connection to database {self.profile.db_name}.")
+        print(f"Opening connection to database {self.profile['name']}.")
         self.connection = psycopg2.connect(
             dbname=self.profile['name'],
             user=self.profile['user'],
             password=self.profile['password'],
-            host=self.profile.host['host'],
+            host=self.profile['host'],
             port=self.profile['port']
         )
         print(f"Connection established. ")
@@ -24,7 +24,11 @@ class ConnectionManager:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.commit()
         self.connection.close()
-        print(f"Connection to database {self.profile.db_name} closed.")
+        print(f"Connection to database {self.profile['name']} closed.")
 
     def commit(self):
         self.connection.commit()
+
+
+def connection_manager(profile=DB_CONFIG) -> ConnectionManager:
+    return ConnectionManager(profile)
