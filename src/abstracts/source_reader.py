778e6decd130f4221d68ced1cdc183a673347df2
Lifeof31415926535 <tpray84@gmail.com>
2023-03-06 14:10:09 -0600

    Divided the sources into small batches which can be iterated.

--- src/abstracts/source_reader.py
+++ src/abstracts/source_reader.py
@@ -1,14 +1,27 @@
 from abc import ABC, abstractmethod
 
-from src.data.filining_sources import FilingSource
-
 
 class SourceReader(ABC):
-    @abstractmethod
-    def initialize(self, start, end, batch_size: int):
-        pass
+    def __init__(self, batch_size):
+        self._sources = []
+        self._current = 0
+        self._batch_size = batch_size
+        self._initialize()
+
+    def __iter__(self):
+        batch = self.next_batch()
+        while batch is not None:
+            yield batch
+            batch = self.next_batch()
 
     @abstractmethod
-    def next_batch(self) -> list[FilingSource] | None:
+    def _initialize(self):
         pass
 
+    def next_batch(self) -> list | None:
+        if self._current >= len(self._sources):
+            return
+        next_idx = self._current + self._batch_size
+        batch = self._sources[self._current: next_idx]
+        self._current = next_idx
+        return batch
