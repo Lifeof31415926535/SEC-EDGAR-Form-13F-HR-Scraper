d92ff94ab87005ce9169191265f788c4900e2ad4
Lifeof31415926535 <tpray84@gmail.com>
2023-03-06 14:24:09 -0600

    Created factory for source readers.

--- src/index_sources/__init__.py
+++ src/index_sources/__init__.py
@@ -1,4 +1,6 @@
+from abstracts import SourceReader
 from forms_index_files import FormsIndexFileManager
+from form_index_source_readers import PostgresFormSourceReader
 
 
 class IndexFileManagerFactory:
@@ -8,4 +10,13 @@ class IndexFileManagerFactory:
             case 'default' | 'Default' | 'forms' | 'Forms':
                 return FormsIndexFileManager(**settings)
             case _:
-                raise Exception()
+                raise Exception('')
+
+
+class IndexSourceReaderFactory:
+    def __call__(self, reader_type: str, batch_size=20, **kwargs) -> SourceReader:
+        match reader_type:
+            case 'default' | 'Default' | 'form-postgres':
+                return PostgresFormSourceReader(batch_size=batch_size, **kwargs)
+            case _:
+                raise Exception('')
